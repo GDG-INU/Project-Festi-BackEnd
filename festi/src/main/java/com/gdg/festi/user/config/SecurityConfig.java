01995aaa5ce0494ae6bbe918449261f53afffe51
Pyeonggeun <rjal3054@gmail.com>
2025-02-27 11:46:33 +0900

    cors 오류

--- festi/src/main/java/com/gdg/festi/user/config/SecurityConfig.java
+++ festi/src/main/java/com/gdg/festi/user/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,12 +24,24 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) //
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/user/login").permitAll(); // 로그인은 인증 없이 가능
-                    auth.requestMatchers("/api/auth/login").permitAll();
+                    auth.requestMatchers("/user/login", "/api/auth/login", "/v1/api/auth/login").permitAll();
                     auth.anyRequest().authenticated(); // 나머지는 인증 필요
                 })
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 모든 도메인 허용
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 모든 HTTP 메서드 허용
+                        .allowCredentials(true); // 인증 정보 포함 요청 허용 (ex. 쿠키, 세션)
+            }
+        };
     }
 }
